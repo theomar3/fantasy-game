59a19f448805da848e7c9ec12d41350a947cc1e4
Omar Ahmad <theomar3@gmail.com>
2016-08-16 13:16:28 -0500

    Added random names and random HP

--- public/script.js
+++ public/script.js
@@ -3,10 +3,38 @@ if (this.Fantasy === undefined) this.Fantasy = {};
 
 (function(context) {
 
+  function generateNames() {
+    var goodNames = ['Goody Gumdrops', 'Hero Number 1', 'Blue Justice', 'Blue Angel'];
+    var badNames = ['Timmay Gingerman', 'Villian Number 1', 'Red Destruction', 'Red Devil'];
+
+    var goodNamesSelector = Math.floor(Math.random() * badNames.length);
+    var badNamesSelector = Math.floor(Math.random() * badNames.length);
+
+    var blueDudeName = goodNames[goodNamesSelector];
+    var redDudeName = badNames[badNamesSelector];
+  }
+
+  function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  function generateHP() {
+    var blueRandomHP = getRandom(5, 10);
+    var redRandomHP = getRandom(5, 10);
+
+    var blueDudeHP = blueRandomHP;
+    console.log('Blue Dude:', blueDudeHP);
+
+
+    var redDudeHP = redRandomHP;
+    console.log('Red Dude:' ,redDudeHP);
+
+  }
+
   function start() {
 
-    //Call your code here
-    console.log('starting!', context);
+    generateNames();
+    generateHP();
 
   }
 
